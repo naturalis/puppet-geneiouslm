604595182ccb19814594527db3af8cdb14665a9a
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-04-06 14:22:14 +0000

    added sensu compatible check script

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ place LMinstaller binary in the files directory.
   $vendorDaemonPort         = 49630,
   $LMinstaller              = 'GeneiousFloatingLicenseManager_linux64_2_0_5.sh',
   $license                  = '<licensenumber>'
+  $geneiousdir              = '/opt/GeneiousFloatingLicenseManager'
+  $chkcritical              = 0,  # return critical (errorlevel 2) on test script then less than x licenses free
+  $chkwarning               = 1,  # return warning (errorlevel 1) on test script then less than x licenses free
 
 ```
 
@@ -29,7 +32,8 @@ Dependencies
 Result
 -------------
 Working license server for geneious.
-
+includes license test script in /usr/local/sbin/chklicense.sh which returns status on condition of licenseserver using lmstat. 
+This test script is usable for sensu montoring checks and will be included in monitoring when role_sensu::client is running on the client. (https://github.com/naturalis/puppet-role_sensu) 
 
 Limitations
 -------------
--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,10 @@ class geneiouslm(
   $licenseServerPort        = 27001,
   $vendorDaemonPort         = 49630,
   $LMinstaller              = 'GeneiousFloatingLicenseManager_linux64_2_0_5.sh',
-  $license                  = 'XXXX-XXXX-XXXX-XXXX-XXXX'
+  $license                  = 'XXXX-XXXX-XXXX-XXXX-XXXX',
+  $geneiousdir              = '/opt/GeneiousFloatingLicenseManager',
+  $chkcritical              = 0,
+  $chkwarning               = 1,
 )
 {
 
@@ -17,68 +20,80 @@ class geneiouslm(
     ensure          => "installed"
   }
 
-  file { "/opt/geneious":
+  file { '/opt/geneious':
     ensure         => 'directory',
     mode           => '0755'
   }
 
   file { 'geneious-installer':
-    path           => "/opt/geneious/geneious-installer.sh",
+    path           => '/opt/geneious/geneious-installer.sh',
     source         => "puppet:///modules/geneiouslm/${LMinstaller}",
-    ensure         => "present",
+    ensure         => 'present',
     mode           => '0755',
     require        => File['/opt/geneious']
   }
   
   file { 'install-answers':
-    path           => "/opt/geneious/install-answers",
-    source         => "puppet:///modules/geneiouslm/install-answers",
-    ensure         => "present",
+    path           => '/opt/geneious/install-answers',
+    source         => 'puppet:///modules/geneiouslm/install-answers',
+    ensure         => 'present',
     mode           => '0644',
     require        => File['/opt/geneious']
   }
 
   exec { 'install geneious':
-    cwd            => "/opt/geneious",
-    command        => "/opt/geneious/geneious-installer.sh < /opt/geneious/install-answers",
-    unless         => "/usr/bin/test -d /opt/GeneiousFloatingLicenseManager",
+    cwd            => '/opt/geneious',
+    command        => '/opt/geneious/geneious-installer.sh < /opt/geneious/install-answers',
+    unless         => "/usr/bin/test -d ${geneiousdir}",
     returns        => [0,1,2,14],
     require        => [File['geneious-installer'],File['install-answers'],Package['lsb'],Package['openjdk-7-jre']],
   }
 
   file { 'geneious dir':
-    path           => "/opt/GeneiousFloatingLicenseManager",
-    ensure         => "present",
+    path           => $geneiousdir,
+    ensure         => 'present',
     mode           => '0777',
     require        => Exec['install geneious']
   }
 
-  
   exec { 'install geneious service':
-    command        => "/opt/GeneiousFloatingLicenseManager/linux64/InstallService.sh",
-    unless         => "/usr/bin/test -f /etc/init.d/geneiouslm",
+    command        => "${geneiousdir}/linux64/InstallService.sh",
+    unless         => '/usr/bin/test -f /etc/init.d/geneiouslm',
     require        => Exec['install geneious']
   }
 
   exec { 'check geneious':
-    command        => "/opt/GeneiousFloatingLicenseManager/floatingLicenseManager -check",
-    unless         => "/opt/GeneiousFloatingLicenseManager/floatingLicenseManager -check | grep -c '${license}'",
+    command        => "${geneiousdir}/floatingLicenseManager -check",
+    unless         => "${geneiousdir}/floatingLicenseManager -check | grep -c '${license}'",
     require        => Exec['install geneious']
   }->
   exec { 'activate geneious':
-    command        => "/opt/GeneiousFloatingLicenseManager/floatingLicenseManager -activate -activationID ${license}",
-    unless         => "/opt/GeneiousFloatingLicenseManager/floatingLicenseManager -check | grep -c '${license}'",
+    command        => "${geneiousdir}/floatingLicenseManager -activate -activationID ${license}",
+    unless         => "${geneiousdir}/floatingLicenseManager -check | grep -c '${license}'",
   }->
   exec { 'install geneious license':
-    command        => "/opt/GeneiousFloatingLicenseManager/floatingLicenseManager -install -licenseServerPort ${licenseServerPort} -vendorDaemonPort ${vendorDaemonPort} -activationID ${license}",
-    unless         => "/usr/bin/test -f /opt/GeneiousFloatingLicenseManager/geneious.lic",
+    command        => "${geneiousdir}/floatingLicenseManager -install -licenseServerPort ${licenseServerPort} -vendorDaemonPort ${vendorDaemonPort} -activationID ${license}",
+    unless         => "/usr/bin/test -f ${geneiousdir}/geneious.lic",
     require        => File['geneious dir']
   }
 
-
   service { 'geneiouslm':
-    ensure         => "running",
-    enable         => "true",
+    ensure         => 'running',
+    enable         => 'true',
     require        => [Exec['install geneious license'],File['geneious dir']]
   }
+
+# create licencemanager check script for usage with monitoring tools ( sensu )
+  file {'/usr/local/sbin/chklicense.sh':
+    ensure         => 'file',
+    mode           => '0777',
+    content        => template('geneiouslm/chklicense.sh.erb')
+  }
+
+# export check so sensu monitoring can make use of it
+  @sensu::check { 'Check Geneious License server status' :
+    command        => '/usr/local/sbin/chklicense.sh',
+    tag            => 'central_sensu',
+  }
+
 }
